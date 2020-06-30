5d09c75a1e89d534d5378bf035595c8c567de166
Luan Coleto <luancoleto@outlook.com>
2020-06-30 13:54:26 -0300

    Create and show transations

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,22 +1,44 @@
 import { Router } from 'express';
-
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
+import { getCustomRepository } from 'typeorm';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+  const transactions = await transactionsRepository.find();
+  const balance = await transactionsRepository.getBalance();
+
+  return response.json({ transactions, balance });
 });
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  const { title, value, type, category } = request.body;
+
+  const createTransactions = new CreateTransactionService();
+
+  const transaction = await createTransactions.execute({
+    title,
+    value,
+    type,
+    category,
+  });
+
+  return response.json(transaction);
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+  const deleteTransaction = new DeleteTransactionService();
+
+  const { id } = request.params;
+
+  await deleteTransaction.execute({ id });
+
+  return response.json({ success: 'transaction deleted' });
 });
 
 transactionsRouter.post('/import', async (request, response) => {
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,42 @@
-// import AppError from '../errors/AppError';
-
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    type,
+    value,
+    title,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    if (!['income', 'outcome'].includes(type)) {
+      throw new AppError('Type is invalid');
+    }
+
+    const { total } = await transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new AppError('You dont have enough balance');
+    }
+    const transaction = await transactionsRepository.create({
+      type,
+      value,
+      title,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
